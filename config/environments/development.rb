cd7745e841b4d24e2b3725cd07854d6b267af63e
Jelani Woods <jelani@firstdraft.com>
2023-04-12 12:53:50 -0500

    Clear hosts in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts.clear
   config.web_console.whitelisted_ips = "0.0.0.0/0.0.0.0"
   BetterErrors::Middleware.allow_ip! "0.0.0.0/0.0.0.0"
   # Settings specified here will take precedence over those in config/application.rb.

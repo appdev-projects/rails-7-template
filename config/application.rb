cb91bf51285d62b12d252094fd90a4c1ef68b536
Ben Purinton <ben.purinton@gmail.com>
2023-07-25 11:16:41 -0800

    updates for AD1 config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,18 @@ module VanillaRails7
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework nil
+      g.factory_bot false
+      g.scaffold_stylesheet false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+    end
+
+    config.action_controller.default_protect_from_forgery = false
+    config.active_record.belongs_to_required_by_default = false
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end

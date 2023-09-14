e62fb01788c68a0a4fd8f89f9df59c14df21bbcb
Ben Purinton <ben.purinton@gmail.com>
2023-09-14 11:36:07 -0800

    Avoid authenticity error in codespaces dev (#17)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow POST authenticity on Codespaces in dev
+  config.action_controller.forgery_protection_origin_check = false
 end

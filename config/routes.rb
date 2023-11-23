151e053ae5aee06932b3356cc73559f0b362a87a
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-23 23:24:21 +0000

    added 'create' waitlist_entries resource to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :retailers
   resources :snapshots
   resources :products
+  resources :waitlist_entries, only[:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

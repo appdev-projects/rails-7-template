af94d2e6cab5daced298a1cdf99d1f05c963d140
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-23 23:28:22 +0000

    defined create action on waitlist_entries_controller

--- app/controllers/waitlist_entries_controller.rb
+++ app/controllers/waitlist_entries_controller.rb
@@ -1,2 +1,16 @@
 class WaitlistEntriesController < ApplicationController
+  def create
+    @waitlist_entry = WaitlistEntry.new(waitlist_entry_params)
+    if @waitlist_entry.save
+      flash[:notice] = 'Successfully added to the waitlist!'
+    else
+      flash[:alert] = 'Error adding to the waitlist. Please try again.'
+    end
+    redirect_to root_path 
+
+    private
+
+  def waitlist_entry_params
+    params.require(:waitlist_entry).permit(:name, :email)
+  end
 end

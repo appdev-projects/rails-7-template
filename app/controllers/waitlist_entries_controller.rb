c14acf9631790f38421f814d20b2ef14af433e25
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-24 00:42:01 +0000

    updated create action on waitliestEntriesController

--- app/controllers/waitlist_entries_controller.rb
+++ app/controllers/waitlist_entries_controller.rb
@@ -7,6 +7,7 @@ class WaitlistEntriesController < ApplicationController
       flash[:alert] = 'Error adding to the waitlist. Please try again.'
     end
     redirect_to root_path 
+  end  
 
     private
 

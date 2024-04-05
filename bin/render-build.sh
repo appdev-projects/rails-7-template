b7dad32f0fc627dfb4df75ba0fbc61a5a96249a4
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-04-05 22:01:32 +0000

    refresh credentials

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ set -o errexit
 bundle install
 
 # For Ruby on Rails apps uncomment these lines to precompile assets and migrate your database.
-# bundle exec rake assets:precompile
-# bundle exec rake assets:clean
-# bundle exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate

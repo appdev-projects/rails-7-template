d6f028f6dcab02ea918b288ca6ca8e875d92bf7f
Benjamin Guzman-Ayala <150963859+BenjaminGuzman-Ayala@users.noreply.github.com>
2024-04-05 22:46:13 +0000

    migration order

--- db/migrate/20240403194948_program_students.rb
+++
@@ -1,9 +0,0 @@
-class ProgramStudents < ActiveRecord::Migration[7.0]
-  def change
-      create_table :programs_students, id: false do |t|
-        t.belongs_to :program
-        t.belongs_to :student
-        t.timestamps
-      end
-  end
-end

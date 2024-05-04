86011b6a70b1753ffb40810fd32822e583ff1424
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2024-05-04 11:20:20 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  # get("/your_first_screen", { :action => "", :controller => "" }
+  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
+
+  # get "/your_first_screen" => "pages#first"
   
 end

b48301754de2d6263e31cb5a4618cf975efc1c2c
Ben Purinton <ben.purinton@gmail.com>
2024-06-05 17:02:53 -0400

    Temporary fix for missing callback on rails_db gem (#41)
    While working on resurrecting the `dev_toolbar` gem, I happened across an issue for the (very popular with our beginners) show-stopper bug in the `rails_db` gem for Rails v7.1: https://github.com/igorkasyanchuk/rails_db/issues/134
    
    I opened a teeny, tiny PR to fix that Gem (https://github.com/igorkasyanchuk/rails_db/pull/139; my first open-source Ruby / Rails contribution 🥳 )
    
    There is not so much activity on that Gem page though, so as a workaround to allow `/rails/db` to function ASAP, we can just disable the default Rails 7.1 setting (in development only, shouldn't affect anything as we don't use callbacks much right now in the Phase 1 or 2 content?)
    
    We can revert this commit when the `rails_db` Gem update is made (I'll make a note of it).

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  config.action_controller.raise_on_missing_callback_actions = false
 
   # Allow POST authenticity on Codespaces in dev
   config.action_controller.forgery_protection_origin_check = false

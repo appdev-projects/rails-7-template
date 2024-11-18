b141577df961adde1d597389644ec7f62738959b
dgarcia13-99 <danielagarcia13.99@gmail.com>
2024-11-18 16:42:49 -0600

    edited main's routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"

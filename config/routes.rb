a4f361b2c812438ea71f401242d429c18f79d142
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-11-19 04:22:54 +0000

    started on user's page

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   resources :survey_responses
   resources :surveys
   resources :reviews
+  resources :users, only: :show
 
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
+  get "/users/:id" => "users#show", as: :user
 end

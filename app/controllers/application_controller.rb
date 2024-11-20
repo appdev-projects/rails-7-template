7c16b93424242e7a473131e57c48f7ea33836035
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-11-20 19:07:14 +0000

    started on Edit Profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :email, :last_name, :avatar,:age, :gender, :occupation, :location, :budget, :bio, :instagram_url, :facebook_url, :video_url, :password, :password_confirmation, :current_password)
+    end
+  end
 end

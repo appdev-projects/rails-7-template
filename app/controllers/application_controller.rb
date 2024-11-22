02b57e67c5602c045dd2227183deaff4b3f4e23b
dgarcia13-99 <danielagarcia13.99@gmail.com>
2024-11-22 17:39:35 -0600

    fixed issues with duplicate models and ensured Turbo is supported to allow for accessing delete and patch paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
 
-  #before_action :authenticate_user!
+  before_action :authenticate_employee!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:employee_number, :email, :first_name, :last_name, :role_id, :store_number])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:employee_number, :email, :first_name, :last_name, :role_id, :shop_number])
 
     devise_parameter_sanitizer.permit(:account_update, :keys => [:email, :first_name, :last_name])
   end

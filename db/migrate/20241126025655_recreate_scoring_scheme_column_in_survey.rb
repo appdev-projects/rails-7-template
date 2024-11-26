2a037c62db177a11766a75e29cef08ac435d5a14
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-11-26 23:49:38 +0000

    test

--- db/migrate/20241126025655_recreate_scoring_scheme_column_in_survey.rb
+++ db/migrate/20241126025655_recreate_scoring_scheme_column_in_survey.rb
@@ -1,5 +1,5 @@
 class RecreateScoringSchemeColumnInSurvey < ActiveRecord::Migration[7.1]
   def change
-    change_column :surveys, :scoring_scheme, :json, using: 'scoring_scheme::JSON'
+    change_column(:surveys, :scoring_scheme, :json, {:using=>"scoring_scheme::JSON"})
   end
 end

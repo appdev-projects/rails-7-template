8dace3bff7892b0730899a31ce16ee6b6c8eb3b0
dgarcia13-99 <danielagarcia13.99@gmail.com>
2024-11-26 22:23:16 -0600

    fixed parameter issue to give devise access to shop_id for employee sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:employee_number, :email, :first_name, :last_name, :role_id, :shop_number])
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:employee_number, :email, :first_name, :last_name, :role_id, :shop_id])
 
     devise_parameter_sanitizer.permit(:account_update, :keys => [:email, :first_name, :last_name])
   end

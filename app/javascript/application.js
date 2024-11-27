765949e3755e68a34a52151aaf7d72ab52afd4af
dgarcia13-99 <danielagarcia13.99@gmail.com>
2024-11-27 10:44:38 -0600

    added jquery to application.js file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import jquery from "jquery";
+window.jQuery = jquery;
+window.$ = jquery;
+import Rails from "@rails/ujs"
+Rails.start();
+
+//= require jquery
+//= require jquery_ujs
+//= require rails-ujs

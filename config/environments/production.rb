c35e2a0ed789b745a5fcfc1b548abf188c90955c
dgarcia13-99 <danielagarcia13.99@gmail.com>
2024-11-27 15:39:18 -0600

    added configuration to compile assets and fixed seeds.db file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.assets.compile = true
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
--- db/seeds.rb
+++ db/seeds.rb
@@ -44,18 +44,18 @@ employees = [
     { first_name: "Sophia", last_name: "Garcia", employee_number: 999999, email: "sophia@sephora.com", password: "password", role_id: Role.find_by(title: "Operations Associate").id, shop_id: Shop.find_by(shop_number: 189).id, admin: false},
     { first_name: "Admin", last_name: "Sephora", employee_number: 100000, email: "admin@sephora.com", password: "password", role_id: Role.find_by(title: "Admin").id, shop_id: Shop.find_by(shop_number: 111).id, admin: true}
   ]
-
-employees.each do |employee|
-  Employee.find_or_create_by(employee_number: employee[:employee_number]) do |e|
-    e.first_name = employee[:first_name].capitalize
-    e.last_name = employee[:last_name].capitalize
-    e.email = employee[:email]
-    e.password = "password"
-    e.role_id = employee[:role_id]
-    e.shop_id = employee[:shop_id]
-    e.admin = employee[:admin]
+  employees.each do |employee|
+    Employee.create(
+      first_name: employee[:first_name].capitalize,
+      last_name: employee[:last_name].capitalize,
+      employee_number: employee[:employee_number],
+      email: employee[:email],
+      password: employee[:password],
+      role_id: employee[:role_id],
+      shop_id: employee[:shop_id],
+      admin: employee[:admin]
+    )
   end
-end
 
 # Brand sample data
 brands = ["Dior", "Merit", "Phlur", "Pattern", "The Inkey List", "Paco Rabanne", "One/Size", "Laura Mercier", "Ceremonia", "Haus Labs", "Charlotte Tilbury", "Summer Fridays", "Tower 28 Beauty", "Benefit Cosmetics", "Dae", "Glossier", "Gucci", "Fenty Beauty", "Youth To The People", "The Ordinary" ]

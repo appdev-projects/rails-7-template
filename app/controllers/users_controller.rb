1ed9d0dce89f365a2fd51faaf14466354f9cfc27
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-12-03 02:23:44 +0000

    Fixed sign in issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def home
     @users = User.all
   end
@@ -9,19 +8,23 @@ class UsersController < ApplicationController
     @users = @q.result.page(params[:page]).per(8)
 
     @breadcrumbs = [
-      {content: "Home", href: "/"},
-      {content: "Pals", href: users_path},
+      { content: "Home", href: "/" },
+      { content: "Pals", href: users_path },
     ]
   end
 
   def show
     @user = User.find_by!(id: params.fetch(:id))
-    @message = Message.new(:sender_id => current_user.id, :recipient_id => @user.id)
+    if current_user
+      @message = Message.new(:sender_id => current_user.id, :recipient_id => @user.id)
+    else
+      @message = nil # Ensure no message is initialized if the user is not signed in
+    end
 
     @breadcrumbs = [
-      {content: "Home", href: "/"},
-      {content: "Pals", href: users_path},
-      {content: @user.pal_name},
+      { content: "Home", href: "/" },
+      { content: "Pals", href: users_path },
+      { content: @user.pal_name },
     ]
   end
 

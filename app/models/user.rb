ec1e1902e27bcc11f7b7579c679ae57bb8c709a8
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-12-09 17:49:20 +0000

    deleted oauth with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,18 +13,15 @@
 #  facebook_url           :string
 #  first_name             :string
 #  gender                 :string
-#  github_access_token    :string
 #  instagram_url          :string
 #  last_name              :string
 #  location               :string
 #  occupation             :string
-#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  survey_score           :integer
 #  trust_score            :integer          default(0)
-#  uid                    :string
 #  video_url              :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
@@ -41,7 +38,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[github]
 
   before_save :calculate_trust_score
 
@@ -53,13 +49,6 @@ class User < ApplicationRecord
   validates :facebook_url, format: { with: /\Ahttps:\/\/www\.facebook\.com\// }, allow_blank: true
   validates :video_url, format: { with: /\Ahttps:\/\/www\.youtube\.com\// }, allow_blank: true
 
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.github_access_token = auth.credentials.token
-    end
-  end
 
   def self.ransackable_attributes(auth_object = nil)
     [ "age", "budget", "gender", "location", "occupation" ]
--- env_test.rb
+++
@@ -1,4 +0,0 @@
-require "dotenv/load"
-
-pp ENV.fetch("GITHUB_ID")
-pp ENV.fetch("GITHUB_SECRET")
